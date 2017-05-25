efd764fe515c4513a1f75c024e25e9a7af5ccc86
Phillip Sauerbeck <pSauerbeck@gmail.com>
2017-05-24 22:24:44 -0700

    make entire top bar draggable, except inputs

--- app/main.js
+++ app/main.js
@@ -18,12 +18,10 @@ let mainWindowLastTitle;
 const platform = process.platform;
 
 function makeDraggable() {
-  if (platform == 'linux') {
-      mainWindow.webContents.insertCSS('paper-toolbar { -webkit-app-region: drag; }');
-      mainWindow.webContents.insertCSS('paper-toolbar .gb_Ie.gb_T.gb_X { -webkit-app-region: no-drag; }');
-      mainWindow.webContents.insertCSS('paper-toolbar #input { -webkit-app-region: no-drag; }');
-      mainWindow.webContents.insertCSS('paper-toolbar paper-icon-button { -webkit-app-region: no-drag; }');
-      }
+  mainWindow.webContents.insertCSS('paper-toolbar { -webkit-app-region: drag; }');
+  mainWindow.webContents.insertCSS('paper-toolbar .gb_Ie.gb_T.gb_X { -webkit-app-region: no-drag; }');
+  mainWindow.webContents.insertCSS('paper-toolbar #input { -webkit-app-region: no-drag; }');
+  mainWindow.webContents.insertCSS('paper-toolbar paper-icon-button { -webkit-app-region: no-drag; }');
 }
 
 function stylePlayer() {
@@ -31,36 +29,6 @@ function stylePlayer() {
 
   mainWindow.webContents.insertCSS('#player paper-icon-button[data-id="show-miniplayer"] { display: none; }');
   mainWindow.webContents.insertCSS(theme);
-}
-
-function getCurrentSong() {
-  var windowTitle = mainWindow.getTitle();
-  console.log("Change: " + windowTitle);
-
-  if(typeof windowTitle === "undefined" || windowTitle === null || _.trim(windowTitle).trim() === "" || windowTitle === mainWindowLastTitle) return null;
-
-  var ignoreTitles = [/^listen.{0,1}now.{0,10}google.{0,1}play.{0,1}music$/i, /^loop$/i, /^google.{0,1}play.{0,1}music$/i];
-  for(var i = 0; i < ignoreTitles.length; i++) {
-    if (ignoreTitles[i].exec(windowTitle) !== null) return null;
-  }
-
-  console.log("Last: " + mainWindowLastTitle);
-  mainWindowLastTitle = windowTitle;
-  console.log("Current: " + windowTitle);
-
-  var titleRegex = /^(.+) - (.+) - Google Play Music$/;
-  var m;
-
-  if ((m = titleRegex.exec(windowTitle)) !== null) {
-    return { artist: m[2], song: m[1] };
-  } else {
-    return null;
-  }
-}
-
-function triggerNotification(_content) {
-  if (mainWindow.isFocused()) return;
-  mainWindow.webContents.executeJavaScript('new Notification("' + _content.title + '", { body: "' + _content.body + '"});');
 }
 
 function updateImage() {
@@ -104,9 +72,7 @@ app.on('ready', function () {
   })
   .then(function() {
     mainWindow.webContents.on('dom-ready', function() {
-      mainWindow.webContents.executeJavaScript('Notification.requestPermission();');
       stylePlayer();
-
       makeDraggable();
     });
 
@@ -120,14 +86,6 @@ app.on('ready', function () {
 
     mainWindow.webContents.on('did-navigate-in-page', function() {
       setTimeout(function(){ updateImage(); }, 1000);
-    });
-
-    mainWindow.on('page-title-updated', function() {
-      var currentSong = getCurrentSong();
-      if(typeof currentSong !== "undefined" && currentSong !== null) {
-        console.log(currentSong);
-        // triggerNotification({ title: currentSong.artist, body: currentSong.song });
-      }
     });
   })
   .then(function() {
